69320956b77dd9f2c3d5b605735b0c35b3346681
asmgrinder <asmgrinder@gmail.com>
2020-09-26 14:54:42 +0300

    night sky

---
+++ Assets/NightSky.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightSky : MonoBehaviour
+{
+	protected float time = 0;
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
+
+    // Update is called once per frame
+	void Update()
+	{
+		time += Time.deltaTime;
+		transform.rotation = Quaternion.AngleAxis(time, Vector3.up);//Quaternion.Euler(time, time, time);
+	}
+}
